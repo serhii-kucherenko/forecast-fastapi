efec8e780102bfed804aa3bb012dd0f958a2407c
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 18:14:05 +0200

    Add configure and prod env support

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ from api import weather
 from views import home
 
 api = fastapi.FastAPI()
-api.mount('/static', StaticFiles(directory='static'), name='static')
 
-api.include_router(home.router)
-api.include_router(weather.router)
 
+def configure():
+    configure_routing()
+
+
+def configure_routing():
+    api.mount('/static', StaticFiles(directory='static'), name='static')
+    api.include_router(home.router)
+    api.include_router(weather.router)
+
+
+# In development
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api, port=8000, host='127.0.0.1')
+# In production
+else:
+    configure()
