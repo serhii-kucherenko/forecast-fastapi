36b5c5748db61663197ae786b580e487848aa42d
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 19:16:49 +0200

    Add forecast caching

--- services/open_weather_service.py
+++ services/open_weather_service.py
@@ -3,6 +3,8 @@ from typing import Optional
 import httpx
 import requests
 
+from infrastructure import weather_cache
+
 api_key: Optional[str] = None
 
 
@@ -22,6 +24,9 @@ def get_report(city: str, state: Optional[str], country: str, units: str) -> dic
 
 
 async def get_report_async(city: str, state: Optional[str], country: str, units: str) -> dict:
+    if forecast := weather_cache.get_weather(city, state, country, units):
+        return forecast
+
     if state:
         q = f'{city},{state},{country}'
     else:
@@ -35,5 +40,7 @@ async def get_report_async(city: str, state: Optional[str], country: str, units:
 
     data = response.json()
     forecast = data["main"]
+
+    weather_cache.set_weather(city, state, country, units, forecast)
 
     return forecast
