413e52da4581f52c667d8dbf7eff897d0ba3056f
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 18:03:21 +0200

    Organize application parts

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import fastapi
 import uvicorn
+from starlette.staticfiles import StaticFiles
+
+
+from api import weather
+from views import home
 
 api = fastapi.FastAPI()
+api.mount('/static', StaticFiles(directory='static'), name='static')
 
-
-@api.get('/')
-def index():
-    return "Hello world"
-
+api.include_router(home.router)
+api.include_router(weather.router)
 
 if __name__ == '__main__':
     uvicorn.run(api, port=8000, host='127.0.0.1')
