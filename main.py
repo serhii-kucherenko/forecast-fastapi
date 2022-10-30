b586bbad85c1b46e9f996fd6a8e0f8c3e7db548f
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 23:58:06 +0200

    Remove fake data

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 from pathlib import Path
 
@@ -8,8 +7,7 @@ from starlette.staticfiles import StaticFiles
 
 
 from api import weather
-from models.location import Location
-from services import open_weather_service, reports_service
+from services import open_weather_service
 from views import home
 
 api = fastapi.FastAPI(
@@ -21,7 +19,6 @@ api = fastapi.FastAPI(
 def configure():
     configure_routing()
     configure_weather_api_keys()
-    configure_fake_data()
 
 
 def configure_routing():
@@ -41,24 +38,6 @@ def configure_weather_api_keys():
     with open(file_path) as settingsFile:
         settings = json.load(settingsFile)
         open_weather_service.api_key = settings['api_key']
-
-
-def configure_fake_data():
-    # This was added to make it easier to test the weather event reporting
-    # We have /api/reports but until you submit new data each run, it's missing
-    # So this will give us something to start from.
-
-    # Changed this from the video due to changes in Python 3.10:
-    # DeprecationWarning: There is no current event loop, loop = asyncio.get_event_loop()
-    loop = asyncio.new_event_loop()
-
-    try:
-        loc = Location(city="Lviv", country="UA")
-        loop.run_until_complete(reports_service.add_report("Misty sunrise today, beautiful!", loc))
-        loop.run_until_complete(reports_service.add_report("Clouds over downtown.", loc))
-    except RuntimeError:
-        print("Fake starter data will no appear on home page.")
-        print("Once you add data with the client, it will appear properly.")
 
 
 # In development
