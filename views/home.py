becc0a4fcc8edf0c6bf01c35b58b15823a496ab5
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 20:34:33 +0200

    Show reports on the home page

--- views/home.py
+++ views/home.py
@@ -2,13 +2,18 @@ import fastapi
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+from services import reports_service
+
 templates = Jinja2Templates(directory="templates")
 router = fastapi.APIRouter()
 
 
 @router.get('/')
-def index(request: Request):
-    return templates.TemplateResponse('/home/index.html', {'request': request})
+async def index(request: Request):
+    reports = await reports_service.get_reports()
+    data = {'request': request, 'reports': reports}
+
+    return templates.TemplateResponse('/home/index.html', data)
 
 
 @router.get('/favicon.ico')
