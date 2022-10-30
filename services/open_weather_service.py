723cfb63460646e7bf2717e140a6427365066037
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 18:59:21 +0200

    Integrate with openweather api

--- services/open_weather_service.py
+++ services/open_weather_service.py
@@ -1,9 +1,20 @@
 from typing import Optional
 
+import requests
+
 api_key: Optional[str] = None
 
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    q = f'{city},{state},{country}'
+    if state:
+        q = f'{city},{state},{country}'
+    else:
+        q = f'{city},{country}'
+
     url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&units={units}&appid={api_key}"
-    print(url)
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    forecast = data["main"]
+
+    return forecast
