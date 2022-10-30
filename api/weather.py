ad2747198bf40e79f7d40695df7569ba59ff6831
Serhii Kucherenko <serhii.kucherenko@windmill.ch>
2022-10-30 18:23:38 +0200

    Add request params with validation

--- api/weather.py
+++ api/weather.py
@@ -1,8 +1,11 @@
 import fastapi
+from typing import Optional
+from fastapi import Depends
+from models.location import Location
 
 router = fastapi.APIRouter()
 
 
-@router.get('/api/weather')
-def weather():
-    return 'Some weather report'
+@router.get('/api/weather/{city}')
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+    return f"City: {loc.city}, State: {loc.state}, Country: {loc.country}, Units: {units}"
